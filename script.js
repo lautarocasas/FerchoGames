ff11a7b059b39887c128a03cf15d9e7c72dd8def
Lautaro <lautarocasas12@gmail.com>
2024-10-11 22:01:32 -0300

    Añadidos mensajes de alert y prompt

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("hola mundo");
+alert("Bienvenido a FerchoGames!");
+let username = prompt("¿Cual es tu nombre?");
+
+let bienvenida = "Gracias por elegirnos "+username+"!";
+
+alert(bienvenida);
+console.log(bienvenida);
